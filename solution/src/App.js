f5af6e3eb1bc796c8069f1472253be34d6ba264f
Canan Ağbal <agbalcanan8069@gmail.com>
2023-04-06 22:36:16 +0300

    Google Fonts

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,20 +1,14 @@
 
 import './App.scss';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
-import Main from './components/Main/Main';
+
 
 const App = () => {
   return(
-    <div className="App">
-      <Navbar/>
-      <Header/>
-      <Main/>
+    <div className='App'>
+
     </div>
   )
 }
-
-export default App;
 
 
 //yarn add sass  veya npm install sass ile eklendi.
